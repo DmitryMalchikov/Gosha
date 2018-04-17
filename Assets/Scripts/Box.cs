43950b81210986d78f4427bd49b3532845f0def0
SnoopTheDogg <fernanda.pilip@gmail.com>
2018-04-17 19:49:17 +0300

    box Sound

--- Assets/Scripts/Box.cs
+++ Assets/Scripts/Box.cs
@@ -6,6 +6,7 @@ public class Box : MonoBehaviour, IPickable
 {
     public void PickUp()
     {
+        AudioManager.PlayIceCreamPickup();
         GameController.Instance.AddBox();
         gameObject.SetActive(false);
     }
