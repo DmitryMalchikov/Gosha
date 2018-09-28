76167de7703546e14c231100e98ae28aee3e9608
DmitryMalchikov <dimamal1995@gmail.com>
2018-09-28 09:29:11 +0300

    singleton  NullReferenceException free

--- Assets/Scripts/Utils/Singleton.cs
+++ Assets/Scripts/Utils/Singleton.cs
@@ -2,7 +2,27 @@
 
 public class Singleton<T> : MonoBehaviour where T: MonoBehaviour
 {
-    public static T Instance { get; private set; }
+    public static T Instance
+    {
+        get
+        {
+            if (!_instance)
+            {
+                _instance = FindObjectOfType<T>();
+            }
+
+            return _instance;
+        }
+        private set
+        {
+            if (!_instance)
+            {
+                _instance = value;
+            }
+        }
+    }
+
+    private static T _instance;
 
     private void Awake()
     {
